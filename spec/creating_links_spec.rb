ee98be9027e3994ac291131726e569d6f7f616b9
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-14 11:51:37 +0100

    Added functionality to add a link

---
+++ spec/creating_links_spec.rb
@@ -0,0 +1,17 @@
+feature 'Create links' do
+
+  scenario 'I can add a new link' do
+    visit ('/links/new')
+    fill_in 'url', with: 'http://www.zombo.com'
+    fill_in 'title', with: 'This is Zombocom'
+    click_button 'Add link'
+
+    # expect(current_path).to eq '/links'
+    expect(page.status_code).to eq 200
+
+
+    within 'ul#links' do
+      expect(page).to have_content 'This is Zombocom'
+    end
+  end
+end
