a35882ee3f89b725497ec41208923eb12e5fd00e
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-14 11:15:33 +0100

    Completed up to challenge 11

--- Gemfile
+++ Gemfile
@@ -5,5 +5,7 @@ ruby '2.3.1'
 # gem "rails"
 gem 'capybara'
 gem 'rspec'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 gem 'sinatra'
 gem 'rspec-sinatra'
--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
+require_relative './app/models/link.rb'
+
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
